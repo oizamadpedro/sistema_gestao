2c82bcf6c136825e5c37b41ba0d7761ca454f855
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2023-05-12 01:06:42 -0300

    Dica de como gerar a secret_key

--- conf/configuracao.py
+++ conf/configuracao.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
+# chave pode ser gerada por ferramentas como o https://djecrety.ir/ e guardada em arquivo .env
 SECRET_KEY_ENV = os.environ.get("SECRET_KEY_ENV")
