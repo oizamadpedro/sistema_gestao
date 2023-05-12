d967e9f012a4ba842e133671c61a2840b575f98e
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2023-05-12 01:03:03 -0300

    SECRET_KEY protegida em arquivo de variável de ambiente

---
+++ conf/configuracao.py
@@ -0,0 +1,8 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SECRET_KEY_ENV = os.environ.get("SECRET_KEY_ENV")
